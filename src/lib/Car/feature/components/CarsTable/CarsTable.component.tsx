08da77d29530ac99ef9c24035af870209773e72b
Ruben Soler <ruben.soler@mimacom.com>
2023-07-21 17:55:04 +0200

    Feature add cars list

--- src/lib/Car/feature/components/CarsTable/CarsTable.component.tsx
+++ src/lib/Car/feature/components/CarsTable/CarsTable.component.tsx
@@ -1,1 +1,13 @@
-export const CarsTable = () => <div>Cars table</div>;
+import { Car } from '@Car/Domain';
+
+interface CarsTableProps {
+  cars: Car[];
+}
+
+export const CarsTable = ({ cars }: CarsTableProps) => (
+  <div>
+    {cars.map((car) => (
+      <p>{car.name}</p>
+    ))}
+  </div>
+);
--- src/lib/Car/feature/pages/Cars/Cars.page.tsx
+++ src/lib/Car/feature/pages/Cars/Cars.page.tsx
@@ -1,5 +1,6 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
+import { Car } from '@Car/Domain';
 import { CarsTable } from '@Car/Feature';
 import { ContainerContext, GET_CARS_SERVICE } from '@Shared/Infrastructure';
 import { Header } from '@Shared/Ui';
@@ -7,12 +8,13 @@ import { Header } from '@Shared/Ui';
 export const CarsPage = () => {
   const containerCtx = useContext(ContainerContext);
   const getCarsService = containerCtx.resolve(GET_CARS_SERVICE);
+  const [cars, setCars] = useState<Car[]>();
 
   useEffect(() => {
     getCarsService
       .getCars()
       .then((cars) => {
-        console.log(cars);
+        setCars(cars);
       })
       .catch((err) => console.log(err));
   }, [getCarsService]);
@@ -20,7 +22,7 @@ export const CarsPage = () => {
   return (
     <>
       <Header />
-      <CarsTable />
+      <CarsTable cars={cars ?? []} />
     </>
   );
 };
