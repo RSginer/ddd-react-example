554c61397c21a584b0b165cc08524df94d922059
Ruben Soler <ruben.soler@mimacom.com>
2023-07-21 19:17:59 +0200

    Style table

--- src/lib/Car/ui/components/CarsTable/CarsTable.component.tsx
+++ src/lib/Car/ui/components/CarsTable/CarsTable.component.tsx
@@ -8,19 +8,25 @@ interface CarsTableProps {
 }
 
 export const CarsTable = ({ cars }: CarsTableProps) => (
-  <div className="grid grid-cols-4 gap-3">
+  <div className="grid grid-cols-5 gap-3">
+    <CarsTableHeader prop={'IMAGE'} />
     <CarsTableHeader prop={'ID'} />
     <CarsTableHeader prop={'NAME'} />
     <CarsTableHeader prop={'BRAND'} />
-    <CarsTableHeader prop={'IMAGE'} />
+    <CarsTableHeader prop={'ACTIONS'} />
 
     {cars.map((car) => (
       <>
+        <div>
+          <img src={car.pictureUrl} alt={car.name} />
+        </div>
         <CarsTableCell prop={car.id} />
         <CarsTableCell prop={car.name} />
         <CarsTableCell prop={car.brand} />
-        <div>
-          <img src={car.pictureUrl} alt={car.name} />
+
+        <div className="flex flex-col gap-4 items-center">
+          <button className="w-28 px-10 h-10 bg-yellow-500 rounded text-white">Edit</button>
+          <button className="w-28 px-10 h-10 bg-red-500 rounded text-white">Delete</button>
         </div>
       </>
     ))}
