0f26d9e9425231068c2f9e8ae7db73959f52ae92
Ruben Soler <ruben.soler@mimacom.com>
2023-07-21 16:39:56 +0200

    Update UpdateCarHttpService

--- src/lib/Car/infrastructure/http/UpdateCarHttpService.ts
+++ src/lib/Car/infrastructure/http/UpdateCarHttpService.ts
@@ -1,5 +1,5 @@
 import { Car, UpdateCarRepository } from '@Car/Domain';
 
 export class UpdateCarHttpService implements UpdateCarRepository {
-    updateCar = (car: Car) => Promise.resolve({id: '', name: '', brand: '', pictureUrl: ''});
+  updateCar = (car: Car) => Promise.resolve({ id: car.id, name: '', brand: '', pictureUrl: '' });
 }
