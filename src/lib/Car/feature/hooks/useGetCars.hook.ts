e97797c29ab56c091dac083d7e4d450b93e16137
Ruben Soler <ruben.soler@mimacom.com>
2023-07-21 19:03:56 +0200

    Refactor move queryKey to inside a hook

--- src/lib/Car/feature/hooks/useGetCars.hook.ts
+++ src/lib/Car/feature/hooks/useGetCars.hook.ts
@@ -3,9 +3,8 @@ import { useQuery } from 'react-query';
 
 import { ContainerContext, GET_CARS_SERVICE } from '@Shared/Infrastructure';
 
-const QUERY_KEY = 'getCars';
-
 export const useGetCars = () => {
+  const QUERY_KEY = 'getCars';
   const containerCtx = useContext(ContainerContext);
   const getCarsService = containerCtx.resolve(GET_CARS_SERVICE);
   const { isLoading, error, data } = useQuery(QUERY_KEY, () => getCarsService.getCars());
