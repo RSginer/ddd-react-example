24817360f2ac5fc639ed557b3db57abf81b549c9
Ruben Soler <ruben.soler@mimacom.com>
2023-07-24 21:11:24 +0200

    Style sorting

--- src/lib/Car/ui/SortSelect/SortSelect.component.tsx
+++ src/lib/Car/ui/SortSelect/SortSelect.component.tsx
@@ -6,7 +6,7 @@ interface SortSelectProps {
 }
 
 export const SortSelect = ({ onSortPropertyChange, onSortOrderChange }: SortSelectProps) => (
-  <div className="flex gap-4">
+  <div className="flex gap-4 ml-1">
     <div className="flex mb-4 cursor-pointer">
       <label className="underline mr-1" htmlFor="sort">
         Sort by:
